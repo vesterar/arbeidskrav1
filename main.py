faf01008b89e3d9461db6e750f4e8d80f4c5f863
arve <arve.vesteraas@hotmail.com>
2024-11-07 08:30:45 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# variabler brukt for kalulering av kostnader per år
+km_year = 20000
+forsikring_el = 5000
+forsikring_bensin = 7500
+trafikkforsikringsavgift = (3.38*365)
+drivfsoff_el = (0.2*2*km_year)
+drivfsoff_bensin = (1*km_year)
+bom_el = (0.1*km_year)
+bom_bensin = (0.3*km_year)
+
+# kalkulering av årlige kostnader for bensin og elbil
+tot_kost_el = round(forsikring_el + trafikkforsikringsavgift + drivfsoff_el + bom_el)
+tot_kost_bensin = round(forsikring_bensin + trafikkforsikringsavgift + drivfsoff_bensin + bom_bensin)
+diff_bensin_el = round(tot_kost_bensin - tot_kost_el)
+
+# Presentasjon av årlige kostnader
+print(f"Den totale årlige kostnaden for bensinbil er kr {tot_kost_bensin},-")
+print(f"Den totale årlige kostnaden for elbil er kr {tot_kost_el},-")
+print(f"Årlig kostnadsdifferansen mellom bensin og elbil er på kr {diff_bensin_el},-")
