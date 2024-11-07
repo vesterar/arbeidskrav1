10dcd6a8ee76fff31d3f84612a7fcaf0b0b64cab
arve <arve.vesteraas@hotmail.com>
2024-11-07 09:01:59 +0100

    minor text update

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 """
 Arbeidskrav1
-Enkelt program for bergening av kostnader for henholdsvis bensinbil og elbil
+Enkelt program for bergening av kostnader for henholdsvis bensinbil og elbil,
 samt differensen av kostnader mellom biltypene.
 """
 
-# variabler brukt for beregning av kostnader per år
+# variabler brukt for beregning av kostnader per år.
 km_year = 20000
 forsikring_el = 5000
 forsikring_bensin = 7500
-trafikkforsikringsavgift = (3.38*365)
+trafikkforsikringsavgift = (8.38*365)
 drivfsoff_el = (0.2*2*km_year)
 drivfsoff_bensin = (1*km_year)
 bom_el = (0.1*km_year)
 bom_bensin = (0.3*km_year)
 
-# kalkulering av årlige kostnader for bensin og elbil
+# kalkulering av årlige kostnader for bensin og elbil.
 tot_kost_el = round(forsikring_el + trafikkforsikringsavgift + drivfsoff_el + bom_el)
 tot_kost_bensin = round(forsikring_bensin + trafikkforsikringsavgift + drivfsoff_bensin + bom_bensin)
 diff_bensin_el = round(tot_kost_bensin - tot_kost_el)
